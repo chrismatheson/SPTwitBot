52cb9c0c028bb22e37951d787be288a9d83c194a
Chris Matheson <chris@matheson.com>
2012-06-06 09:29:36 +0000

    tryout deploying app

--- App.js
+++ App.js
@@ -1,9 +1,3 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World from Cloud9\n');
-}).listen(process.env.PORT);
-
 console.log("Starting Bot");
 
 var util = require('util'),
