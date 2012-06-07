d5ca5bd8ca630fdecd3357536671c4f3e2a4622e
Chris Matheson <chris@matheson.com>
2012-06-07 12:41:20 +0000

    beta version

--- App.js
+++ App.js
@@ -10,30 +10,79 @@ var twit = new twitter({
     access_token_secret: 'fY4AZRybIfYNLHihThqeR4vlzzS91yZa5CeX38Fumw'
 });
 
-var menu = new Array([  'The Current Menu at @smallplatesnq looks like this',
-                        'BUTTERMILK FRIED CHICKEN w/ roasted peanut sauce\n CRISPY PORK CHEEK w/ smoked bacon sauce & Rekordelig mayo\n LAMB MEATBALLS w/ tomato sauce',
-                        'BREADED WHITEBAIT w/ cockle ‘popcorn’, Rekorderlig mayo\n COD CHEEK IN BEER BATTER w/ mushy peas\n MINI SQUID BURGERS w/ lettuce and onion',
-                        'MARIS PIPER MASH CAKES w/ cucumber ketchup, smoked salt\n YORKSHIRE PUDDINGS w/ whipped blue cheese, chilli jam\n ASPARAGUS SPEARS w/ fondue',
-                        '====== Sides (£3.00) ======',
-                        'TRIPLE COOKED CHIPS w/ served with ketchup and mayo\n ONION RINGS w/ fried in beer batter\n PORK SCRATCHINGS w/ five spice, salt and pepper']);
+var menu = new Array('The Current Menu at @smallplatesnq looks like this',
+                    'BUTTERMILK FRIED CHICKEN w/ roasted peanut sauce\n\n CRISPY PORK CHEEK w/ smoked bacon sauce & Rekordelig mayo',
+                    'LAMB MEATBALLS w/ tomato sauce\n\n BREADED WHITEBAIT w/ cockle ‘popcorn’, Rekorderlig mayo',
+                    'COD CHEEK IN BEER BATTER w/ mushy peas\n\n MINI SQUID BURGERS w/ lettuce and onion',
+                    'MARIS PIPER MASH CAKES w/ cucumber ketchup, smoked salt\n\n YORKSHIRE PUDDINGS w/ whipped blue cheese, chilli jam',
+                    'ASPARAGUS SPEARS w/ fondue',
+                    '====== Sides (£3.00) ======',
+                    'TRIPLE COOKED CHIPS w/ served with ketchup and mayo\n\n ONION RINGS w/ fried in beer batter',
+                    'PORK SCRATCHINGS w/ five spice, salt and pepper');
 
 var currentUserID = null;
 var messageQueue = null;
 
-var processQueue = function(prevMSG) {
-        if (messageQueue.length) {
-            twit.newDirectMessage(currentUserID, messageQueue.shift(), processQueue);
-            console.log('now there are ' + messageQueue.length + ' messages left');
-        }
-        else {
-            messageQueue = null;
-        }
-    };
+var processQueue = function(data) {
+    if(typeof data == 'undefined'){
+        //start of queue
+        data = new Object({});
+    }
+    
+    switch(typeof data)
+    {
+    case 'error':
+      if (data.statusCode != 200){
+        console.error('error sending msg');
+        console.error(util.inspect(data));
+        process.exit(1);
+      }
+      break;
+    default:
+      console.log(data.type);
+      // no data = start of queue so dont need to check respose data
+      if (messageQueue.length) {
+        // Bloody rate limit
+        twit.newDirectMessage(currentUserID, messageQueue.shift(), processQueue);  
+        //twit.updateStatus('@chrismatheson ' + messageQueue.shift(), processQueue);
+        console.log('now there are ' + messageQueue.length + ' messages left');
+      }
+      else {
+        messageQueue = null;
+      }
+    }
+};
+
+/**
+ * Main program starts here
+ */
+var date = new Date();
+twit.updateStatus('Starting Robo Waiter Twitter Bot \n\n'+date,function(){});
+
+twit.rateLimitStatus(function(data){
+    if(!data.remaining_hits){
+        console.error('Panic! : Account has been rate limited');
+        console.error(util.inspect(data));
+    }
+    console.log('Account has '+data.remaining_hits+' hits left.');
+});
+
+console.log('Menu has ' + menu.length + ' items');
+/**
+ * Debug code
+ */
+ currentUserID = 35538233;
+ messageQueue = menu.slice(0); //copy array dont referance
+ console.log('Starting with ' + messageQueue.length + ' messages');
+ processQueue();
+ 
 
 twit.stream('statuses/filter', {'track' : 'mathesonserver showmenu'}, function(stream) {
 	stream.on('data', function(data) {
         currentUserID = data.user.id;
         messageQueue = menu.slice(0); //copy array dont referance
+        console.log('Menu requested by @'+data.user.screen_name);
+        console.log('Starting with ' + messageQueue.length + ' messages');
         processQueue();
     });
 });
